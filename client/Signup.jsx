bee672fa406d5af07853a5027730a12a4fb4a947
Hashim-21 <70618821+Hashim-21@users.noreply.github.com>
2022-11-06 12:16:08 -0600

    styled sign up and connedted to back end

--- client/Signup.jsx
+++ client/Signup.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
+// import logo from './assets/Subify_Logo.png'
 export default function Signup() {
 
 
@@ -52,68 +53,71 @@ export default function Signup() {
   }
 
   return (
-    <div className='form'>
-      <form className='signUpForm' onSubmit={handleSubmit}>
+    <div className= "base-container">
+      <div className='content'>
+      {/* <img src={logo} alt='logo' className='logo' /> */}
+      <form className='form' onSubmit={handleSubmit}>
+      {/* <div className="header">Shopify</div> */}
+      <div className='form-group'>
+        <label htmlFor='username'>Username</label>
         <input 
          type="text"
-         id="usernameSignup" 
-         placeholder="username"
          onChange={(e) => setUsername(e.target.value) } 
          value={username} 
          /> 
-        
+         </div>
+         <div className='form-group'>
+         <label htmlFor='password'>Password</label>
         <input 
          type="password"
-         id="passwordSignup" 
-         placeholder="password"
          onChange={(e) => setPassword(e.target.value) } 
          value={password} 
          /> 
+         </div>
+         <div className='form-group'>
+         <label htmlFor='Firstname'>First Name</label>
         <input 
         type="text" 
         id="firstnameSignup" 
-        placeholder="date" 
-        onChange={(e) => setaccount_date(e.target.value)}
-        value={account_date}
-        />
-        <input 
-        type="text" 
-        id="firstnameSignup" 
-        placeholder="firstname" 
         onChange={(e) => setFirstname(e.target.value)}
         value={first_name}
         />
+        </div>
+         <div className='form-group'>
+         <label htmlFor='lastname'>Last name</label>
         <input
-        type="text" 
-        id="lastnameSignup" 
-        placeholder="lastname" 
+        type="text"  
         onChange={(e) => setLastname(e.target.value)}
         value={last_name}
         />
+        </div>
+         <div className='form-group'>
+         <label htmlFor='location'>
+          Location</label>
         <input 
         type="text" 
-        id="location" 
-        placeholder="location" 
         onChange={(e) => setLocation(e.target.value)}
         value={location} 
         />
-
+           </div>
+           <div className='form-group'>
+           <label htmlFor='email'>Email</label>
         <input 
-        type="text" 
-        id="emailSignup" 
-        placeholder="email" 
+        type="text"  
         onChange={(e) => setEmail(e.target.value)}
         value={email}
          />
-
+         </div>
+        <div className='form-group'>
+        <label htmlFor='phoneNumber'>Phone Number</label>
         <input
          type="text" 
-         id="phonenumberSignup"
-         placeholder="phonenumber" 
          onChange={(e) => setPhoneNumber (e.target.value)}
          value={phone_number} />
+         </div>
         <button id='signupButton'>Sign Up</button>
       </form>
+    </div>
     </div>
   );
 }
