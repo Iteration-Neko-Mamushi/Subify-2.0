a327bf17d1524a8ceb6a2df2d1c250fb651d3fb1
Kelvin Van <kelvinvan.cs@gmail.com>
2022-11-07 09:55:02 -0800

    Improvements to readability   Cleaned up broken/unused code   Removed unnecessary comments   Added comments to help with readability
    Co-authored-by: Kelvin Van <90666201+KelvinVan1@users.noreply.github.com>
    Co-authored-by: James Howat <97859901+jbhowat@users.noreply.github.com>
    Co-authored-by: Ed Cho <106274379+edcho720@users.noreply.github.com>
    Co-authored-by: Yufa Li <112290188+01001101CK@users.noreply.github.com>
    Co-authored-by: Hashim Farah <70618821+Hashim-21@users.noreply.github.com>

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,62 +1,41 @@
-const db = require('../model/subifyModel'); // connect to Model file
+const db = require('../model/subifyModel');
 
 const AuthController = {};
 
+//Controller function which serves to verify the user
 AuthController.verifyUser = (req, res, next) => {
 
-  const { username, password } = req.body;// deconstruct req.body and extract user and pass
-  const queryString = 'SELECT username FROM username WHERE username = $1 AND password = $2;';// construct a query string
-  const params = [username, password]; // store the values in an array called params
+  const { username, password } = req.body;
+  const queryString = 'SELECT username FROM username WHERE username = $1 AND password = $2;';
+  const params = [username, password];
 
-  db.query(queryString, params)// pass in the query string and params
+  db.query(queryString, params)
     .then(result => {
       if(result.rows) {
+        //Console log for debugging purposes
         console.log(result.rows[0]);
+
+        //Check to see if a valid match was found
         if(result.rows.length > 0)
           res.locals.username = {username: result.rows[0].username};
         else
           res.locals.username = {};
-        return next(); // check if user and pass exist in the database
+
+        return next();
       }
     })
     .catch(err => {
       return next(err);
-    }); // if either the user or pass 
-// or we can <return res.send('Incorrect credentials!')>
-// or we can reroute to login page: 
+    }); 
 };
 
-
+//Controller function to set a new cookie
 AuthController.setCookie = (req, res, next) => {
+  //Check to see if a valid login request was found by verifyUser
   if(res.locals.username.username) {
     res.cookie('token', res.locals.username.username);
   }
   return next();
 };
 
-
-// AuthController.verifyCookie = (req, res, next) => {
-
-// const { token } = req.cookies;
-// const queryString = 'SELECT token FROM <what table?> WHERE token = $1;'
-// const params = [token];
-
-// db.query(queryString, params)// pass in the query string and params
-//     .then(result => {
-//         if(result[0] === /* '<user || admin>' */) {
-//             // can console.log something
-//             // reroute to member page
-//             return next();
-//         }
-// }).catch(err => next(err)); 
-
-// };
-
 module.exports = AuthController;
-
-/* module.exports = {
-  query: (text, params, callback) => {
-    console.log('executed query', text);
-    return pool.query(text, params, callback);
-  }
-}; */
